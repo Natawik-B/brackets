54fcceccf5b13d93f685b21547c449bc82bc4c62
Natawik-B <natawwwik37@gmail.com>
2022-02-08 22:11:31 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const brackets1 = ['(', '{', '|', '['];
+  const closingPare = { [')']: '(', ['}']: '{', ['|']: '|', [']']: '[', };
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let bracketsConfig = str[i];
+    if (brackets1.includes(bracketsConfig)) {
+      stack.push(bracketsConfig);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      
+      let topBracketConfig = stack[stack.length - 1];
+      if (closingPare[bracketsConfig] === topBracketConfig) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
