20816bf908387ac08ef47c6f58811bf7598936f3
Natawik-B <natawwwik37@gmail.com>
2022-07-16 21:16:40 +0300

    fix: fix the solution

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-
-  const bracketsOpen = ['(', '{', '|', '['];
-  const closingPare = {[')']: '(', ['}']: '{', ['|']: '|', [']']: '['};
-
   let stack = [];
+  
+  let newBracketsConfig = new Map(bracketsConfig);
 
-  for (let i = 0; i < str.length; i++) {
-    let bracketsConfig = str[i];
-    let topBracketsConfig = stack[stack.length - 1];
-
-    if (bracketsOpen.includes(bracketsConfig)) {
-      if (bracketsConfig === topBracketsConfig && (bracketsConfig === '|' ||bracketsConfig === '7' ||bracketsConfig === '8' ||bracketsConfig === '2' ||bracketsConfig === '5' ||bracketsConfig === '6')) {
-        stack.pop();
-      } else {
-        stack.push(bracketsConfig);
-        }
-      } else {
-      if (stack.length === 0) {
-        return false;
-      }
-      
-      if (closingPare[bracketsConfig] === topBracketsConfig) {
-        stack.pop();
-      } else {
-        return false;
-      }
+  str.split('').forEach((elem) => {
+    if (elem == newBracketsConfig.get(stack[stack.length - 1])) {
+      stack.pop();
+    } else {
+      stack.push(elem);
     }
-  }
-
-  return stack.length === 0;
+  })
+  return stack.length !== 0 ? false : true;
 }
